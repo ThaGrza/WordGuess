dd41c5d7822c69b17f8182531115784748ba1651
Andrew Griswold <andrewgriswold7@gmail.com>
2020-02-20 18:57:47 -0600

    started added game Ui components

---
+++ src/components/Gameui/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Gameui() {
+    return (
+        <div className="container">
+            <h1>Word Guess Game!</h1>
+            <div className="game-container">
+            </div>
+        </div>
+    )
+}
---
+++ src/components/WordVault/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const words = {
+    
+    expelliarmus: {
+        picture: "#",
+        soundbite: "#"
+    },
+    hogwarts: {
+        picture: "#",
+        soundbite: "#"
+    },
+    gryffindor: {
+        picture: "#",
+        soundbite: "#"
+    },
+    slytherin: {
+        picture: "#",
+        soundbite: "#"
+    },
+}
